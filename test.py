cbb9b609553c18895fa986c1ef866e4197bc08be
Axel Vasquez <a.axelvc@gmail.com>
2021-09-02 04:07:00 -0500

    remove go code and update python code

--- test.py
+++ test.py
@@ -6,46 +6,51 @@ gridSize = 9
 gridLength = 81
 
 
+def printS(puzzle):
+    N = 23
+    s = '─' * (N // blockSize)
+
+    print(f'┌{s}┬{s}┬{s}┐')
+
+    for i in range(gridSize):
+        n = i * gridSize
+
+        print(
+            '│',
+            ' '.join(map(str, puzzle[0+n:3+n])),
+            '│',
+            ' '.join(map(str, puzzle[3+n:6+n])),
+            '│',
+            ' '.join(map(str, puzzle[6+n:9+n])),
+            '│',
+        )
+
+        if i == 2 or i == 5:
+            print(f'├{s}┼{s}┼{s}┤')
+
+    print(f'└{s}┴{s}┴{s}┘')
+
+
 class Sudoku():
     solution = [0] * gridLength
+    puzzle = [0] * gridLength
 
     def __init__(self):
         self.create()
 
     def create(self):
-        self.fill()
-
-    def print(self):
-        N = 23
-        s = '─' * (N // blockSize)
-
-        print(f'┌{s}┬{s}┬{s}┐')
-
-        for i in range(gridSize):
-            n = i * gridSize
-
-            print(
-                '│',
-                ' '.join(map(str, self.solution[0+n:3+n])),
-                '│',
-                ' '.join(map(str, self.solution[3+n:6+n])),
-                '│',
-                ' '.join(map(str, self.solution[6+n:9+n])),
-                '│',
-            )
-
-            if i == 2 or i == 5:
-                print(f'├{s}┼{s}┼{s}┤')
-
-        print(f'└{s}┴{s}┴{s}┘')
+        self.cleanGrid()
+        self.fillDiagonal()
+        self.fillBlanks()
+        self.randomClean(35)
 
     # ---------------------------------- utils ---------------------------------
     def randomNums(self):
-        # return [5, 4, 3, 8, 9, 7, 2, 1, 6]
-        arr = list(range(1, gridSize + 1))
-        shuffle(arr)
+        return [5, 4, 3, 8, 9, 7, 2, 1, 6]
+        # arr = list(range(1, gridSize + 1))
+        # shuffle(arr)
 
-        return arr
+        # return arr
 
     def getIndex(self, row, col):
         return row * gridSize + col
@@ -61,40 +66,64 @@ class Sudoku():
         return (self.getIndex(row, col) for row in range(gridSize))
 
     def getBlockIdxs(self, i):
-        rowI = i // gridSize
-        colI = i % gridSize
-        rowStart = rowI // blockSize * blockSize
-        colStart = colI // blockSize * blockSize
-        rowEnd = rowStart + blockSize
-        colEnd = colStart + blockSize
+        row = i // gridSize
+        col = i % gridSize
+        rowStart = row // blockSize * blockSize
+        colStart = col // blockSize * blockSize
 
         return (
-            self.getIndex(row, col)
-            for col in range(colStart, colEnd)
-            for row in range(rowStart, rowEnd)
+            self.getIndex(rowStart + r, colStart + c)
+            for c in range(3)
+            for r in range(3)
         )
+
+    def getRandomIndices(self):
+        indices = list(range(len(self.puzzle)))
+        shuffle(indices)
+
+        return indices
 
     def getValidValues(self, i):
         return (n for n in range(1, gridSize + 1) if self.isValid(i, n))
 
+    def getSolutions(self, limit, found, start):
+        try:
+            i = self.puzzle.index(0, start)
+        except:
+            found.append(self.puzzle[::])
+
+            return found
+
+        for n in self.getValidValues(i):
+            self.puzzle[i] = n
+
+            self.getSolutions(limit, found, i)
+
+            if (len(found) >= limit):
+                break
+
+        self.puzzle[i] = 0
+
+        return found
+
     # -------------------------------- valiation -------------------------------
     def isValidRow(self, i, n):
         for rI in self.getRowIdxs(i):
-            if self.solution[rI] == n:
+            if self.puzzle[rI] == n:
                 return False
 
         return True
 
     def isValidCol(self, i, n):
         for cI in self.getColIdxs(i):
-            if self.solution[cI] == n:
+            if self.puzzle[cI] == n:
                 return False
 
         return True
 
     def isValidBlock(self, i, n):
         for bI in self.getBlockIdxs(i):
-            if self.solution[bI] == n:
+            if self.puzzle[bI] == n:
                 return False
 
         return True
@@ -103,6 +132,9 @@ class Sudoku():
         return self.isValidRow(i, n) and self.isValidCol(i, n) and self.isValidBlock(i, n)
 
     # ---------------------------------- fill ----------------------------------
+    def cleanGrid(self):
+        self.puzzle = [0] * gridLength
+
     def fillDiagonal(self):
         blocks = map(self.getBlockIdxs, [0, 30, 60])
 
@@ -110,37 +142,37 @@ class Sudoku():
             nums = self.randomNums()
 
             for i in block:
-                self.solution[i] = nums.pop()
+                self.puzzle[i] = nums.pop()
 
-    def fillCell(self, i):
-        # end of the grid
-        if i == gridLength:
-            return True
+    def fillBlanks(self):
+        [solution] = self.getSolutions(1, [], 0)
 
-        # pre-filled
-        if (self.solution[i]):
-            return self.fillCell(i + 1)
+        self.puzzle = solution
 
-        for n in self.getValidValues(i):
-            self.solution[i] = n
+    def randomClean(self, limit):
+        self.solution = self.puzzle[::]
 
-            if self.fillCell(i + 1):
-                return True
+        removed = 0
+        for i in self.getRandomIndices():
+            snapshot = self.puzzle[i]
 
-        self.solution[i] = 0
-        return False
+            self.puzzle[i] = 0
 
-    def fill(self):
-        self.solution = [0] * gridLength
-        self.fillDiagonal()
-        self.fillCell(0)
+            if (len(self.getSolutions(2, [], 0)) > 1):
+                self.puzzle[i] = snapshot
+            else:
+                removed += 1
+
+            if (removed == limit):
+                break
 
 
 if __name__ == '__main__':
     t = time()
     s = Sudoku()
 
-    # s.print()
+    # printS(s.solution)
+    # printS(s.puzzle)
 
     for i in range(1000):
         s.create()
